180083b5840f2dc6fa73f23c95474061c33ce32f
sarveshdakare <92318452+sarveshdakare@users.noreply.github.com>
2024-07-31 22:01:54 +0800

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,25 +1,4 @@
-// import { BottomWarning } from "../components/BottomWarning"
-// import { Button } from "../components/Button"
-// import { Heading } from "../components/Heading"
-// import { InputBox } from "../components/InputBox"
-// import { SubHeading } from "../components/SubHeading"
 
-// export const Signin = () => {
-//     return <div className="bg-slate-300 h-screen flex justify-center">
-//     <div className="flex flex-col justify-center">
-//       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-//         <Heading label={"Sign in"} />
-//         <SubHeading label={"Enter your credentials to access your account"} />
-//         <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
-//         <InputBox placeholder="123456" label={"Password"} />
-//         <div className="pt-4">
-//           <Button label={"Sign in"} />
-//         </div>
-//         <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
-//       </div>
-//     </div>
-//   </div>
-// }
 
 import { useState } from "react";
 import { BottomWarning } from "../components/BottomWarning";
@@ -43,7 +22,7 @@ export const Signin = () => {
                     <SubHeading label={"Enter your credentials to access your account"} />
                     <InputBox 
                         onChange={(e) => setUsername(e.target.value)} 
-                        placeholder="harkirat@gmail.com" 
+                        placeholder="xyz@gmail.com" 
                         label={"Email"} 
                     />
                     <InputBox 
